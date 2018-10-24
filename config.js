3b4657ecaaa06b5c98503eafcdc4aad4cabb8b5f
silverfang94 <32561429+silverfang94@users.noreply.github.com>
2018-10-24 20:54:45 +0300

    0.0.3

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 module.exports = {
   PORT: process.env.PORT || 3000,
-  MONGO_URL: 'mongodb://@ds042677.mlab.com:42677/nodeexpress',
+  MONGO_URL:
+    /*'mongodb://cretuion94:8octombrie081094silver_fang@ds042677.mlab.com:42677/nodeexpress', */ 'mongodb://@ds042677.mlab.com:42677/nodeexpress',
   user: 'cretuion94',
   password: '8octombrie081094silver_fang'
 };
