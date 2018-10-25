b6eee87bb8f0b7163b99b0375914377fb4a3a0a7
Ion Cretu <ion@falcon.local>
2018-10-25 12:48:56 +0300

    0.0.4

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const Post = require('./models/post');
 const app = express();
-
-const arr = ['hello', 'world', 'test'];
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extend: true }));
 
-app.get('/', (req, res) => res.render('index', { arr: arr }));
+app.get('/', (req, res) =>
+  Post.find({}).then(posts => res.render('index', { posts: posts }))
+);
 
 app.get('/create', (req, res) => res.render('create'));
 app.post('/create', (req, res) => {
-  arr.push(req.body.text);
+  const { title, body } = req.body;
+  Post.create({
+    title: title,
+    body: body
+  }).then(post => console.log(post.id));
+
   res.redirect('/');
 });
 
