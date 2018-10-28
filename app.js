a9833f9b160e8197bfa96ffec333dad01298fef5
silverfang94 <32561429+silverfang94@users.noreply.github.com>
2018-10-28 16:46:45 +0200

    0.0.6

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const staticAsset = require('static-asset');
 const path = require('path');
 
 const app = express();
@@ -7,6 +8,7 @@ const app = express();
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extend: true }));
+app.use(staticAsset(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 //jQuery
---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+/*eslint-disable node/no-unpublished-require*/
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const cssnano = require('gulp-cssnano');
+const browserSync = require('browser-sync');
+const plumber = require('gulp-plumber');
+const nodemon = require('gulp-nodemon');
+/*eslint-disable node/no-unpublished-require*/
+
+gulp.task('nodemon', function() {
+  nodemon({ script: 'index.js' });
+});
+gulp.task('scss', () => {
+  return gulp
+    .src('dev/scss/**/*.scss')
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(
+      autoprefixer(['last 15 versions', '>1%', 'ie 8', 'ie 7'], {
+        cascade: true
+      })
+    )
+    .pipe(cssnano())
+    .pipe(gulp.dest('public/stylesheets'))
+    .pipe(browserSync.reload({ stream: true }));
+});
+
+gulp.task('browser-sync', () => {
+  browserSync({
+    server: { baseDir: 'views' },
+    notify: false
+  });
+});
+
+gulp.task('default', ['browser-sync', 'scss'], () => {
+  gulp.watch('dev/scss/**/*.scss', ['scss']);
+  gulp.watch('views/**/*.ejs', browserSync.reload);
+});
