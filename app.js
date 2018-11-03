e9fa3c7a3f0dd21b9d478f7deccd3cce5db2a38b
silverfang94 <32561429+silverfang94@users.noreply.github.com>
2018-11-03 19:06:47 +0200

    0.0.8

--- app.js
+++ app.js
@@ -3,8 +3,36 @@ const bodyParser = require('body-parser');
 const staticAsset = require('static-asset');
 const path = require('path');
 
+//database
+const config = require('./config');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+mongoose.set('debug', config.IS_PRODUCTION);
+
+mongoose.connection
+  .on('error', error => console.log(error))
+  .on('close', () => console.log * 'Database connedction closed.')
+  .once('open', () => {
+    const info = mongoose.connections[0];
+    console.log(`Connected to ${info.host}:${info.port}/${info.name}`);
+  });
+
+mongoose.connect(
+  config.MONGO_URL,
+  {
+    useNewUrlParser: true,
+    auth: {
+      user: config.user,
+      password: config.password
+    }
+  }
+);
+
+//express
 const app = express();
 
+//sets and uses
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extend: true }));
@@ -14,8 +42,28 @@ app.use(express.static(path.join(__dirname, 'public')));
 //jQuery
 app.use(express.static(path.join(__dirname, 'node_modules', 'jquery', 'dist')));
 
+//routers
 app.get('/', (req, res) => {
   res.render('index');
 });
 
-module.exports = app;
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+// eslint-disable-next-line no-unused-vars
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.render('error', {
+    message: error.message,
+    error: !config.IS_PRODUCTION ? error : {}
+  });
+});
+
+app.listen(config.PORT, () =>
+  console.log(`Example app listening on port ${config.PORT}!`)
+);
--- config.js
+++ config.js
@@ -1,7 +1,14 @@
+const dotenv = require('dotenv');
+const path = require('path');
+
+const root = path.join.bind(this, __dirname);
+dotenv.config({ path: root('.env') });
+
 module.exports = {
   PORT: process.env.PORT || 3000,
-  MONGO_URL:
-    /*'mongodb://cretuion94:8octombrie081094silver_fang@ds042677.mlab.com:42677/nodeexpress', */ 'mongodb://@ds042677.mlab.com:42677/nodeexpress',
+  MONGO_URL: process.env.MONGO_URL,
+  /*'mongodb://cretuion94:8octombrie081094silver_fang@ds042677.mlab.com:42677/nodeexpress',  'mongodb://@ds042677.mlab.com:42677/nodeexpress',*/
   user: 'cretuion94',
-  password: '8octombrie081094silver_fang'
+  password: '8octombrie081094silver_fang',
+  IS_PRODUCTION: process.env.NODE_ENV === 'production'
 };
